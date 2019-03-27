5e7d5775b0ddb6b7f8b88504fa5abdbeaea99d90
jose.carvajal22 <jacarvajalt@correo.udistrital.edu.co>
2019-03-26 22:56:55 -0500

    Conexion Botones en Backend con botones en interfaz.

--- app/src/main/java/com/example/sistemadechistes/MainActivity.java
+++ app/src/main/java/com/example/sistemadechistes/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.sistemadechistes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnIngresarChiste;
+    Button btnCalificarChiste;
+    Button btnChistesRecomendados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
