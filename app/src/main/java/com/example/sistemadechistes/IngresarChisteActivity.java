446c89f40ebfed41205870a3f720a321057306c3
jose.carvajal22 <jacarvajalt@correo.udistrital.edu.co>
2019-03-26 23:15:13 -0500

    Creación Activity primer Botón y paso entre activitis.

---
+++ app/src/main/java/com/example/sistemadechistes/IngresarChisteActivity.java
@@ -0,0 +1,19 @@
+package com.example.sistemadechistes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class IngresarChisteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar_chistes);
+
+
+
+    }
+
+}
--- app/src/main/java/com/example/sistemadechistes/MainActivity.java
+++ app/src/main/java/com/example/sistemadechistes/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.sistemadechistes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +18,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnIngresarChiste = (Button)findViewById(R.id.btnIngresarChiste);
+
+        btnIngresarChiste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, IngresarChisteActivity.class);
+
+                MainActivity.this.startActivity(intent);
+
+            }
+        });
 
         btnCalificarChiste = (Button)findViewById(R.id.btnCalificarChiste);
 
