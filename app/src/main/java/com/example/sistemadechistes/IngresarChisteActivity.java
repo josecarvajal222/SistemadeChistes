21f08c9f9a02edb76ed0cea9f053391085ff8f69
jose.carvajal22 <jacarvajalt@correo.udistrital.edu.co>
2019-03-28 23:11:32 -0500

    Creación Button Subir Chiste.

--- app/src/main/java/com/example/sistemadechistes/IngresarChisteActivity.java
+++ app/src/main/java/com/example/sistemadechistes/IngresarChisteActivity.java
@@ -2,15 +2,18 @@ package com.example.sistemadechistes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 public class IngresarChisteActivity extends AppCompatActivity {
+
+    Button subirChiste;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar_chistes);
 
-
+        subirChiste = (Button)findViewById(R.id.ButtonSubirChiste);
 
     }
 
