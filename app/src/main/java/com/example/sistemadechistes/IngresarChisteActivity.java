811bdd96ecbbc899ad3be5c99d067975db098025
jose.carvajal22 <jacarvajalt@correo.udistrital.edu.co>
2019-03-30 14:28:41 -0500

    Conexion y creacion de hash map para subir datos a la coleccion en firebase.

--- app/src/main/java/com/example/sistemadechistes/IngresarChisteActivity.java
+++ app/src/main/java/com/example/sistemadechistes/IngresarChisteActivity.java
@@ -1,10 +1,21 @@
 package com.example.sistemadechistes;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class IngresarChisteActivity extends AppCompatActivity {
 
@@ -30,6 +41,8 @@ public class IngresarChisteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar_chistes);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
         subirChiste = (Button)findViewById(R.id.ButtonSubirChiste);
 
         tituloChiste = (EditText)findViewById(R.id.txtTituloChiste);
@@ -46,11 +59,19 @@ public class IngresarChisteActivity extends AppCompatActivity {
 
         instrucciones = (EditText)findViewById(R.id.txtInstrucciones);
 
+        final Map<String, Object> chiste = new HashMap<>();
+
         subirChiste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
+                chiste.put("chiste",ingresoChiste.getText().toString());
+                chiste.put("gradoDiversion",Integer.parseInt(gradoDiversion.getText().toString()));
+                chiste.put("gradoNovedad",Integer.parseInt(gradoNovedad.getText().toString()));
+                chiste.put("gradoOportuno",Integer.parseInt(gradoOportuno.getText().toString()));
+                chiste.put("instrucciones",instrucciones.getText().toString());
+                chiste.put("personajes",personajes.getText().toString());
+                chiste.put("titulo",tituloChiste.getText().toString());
 
             }
         });
