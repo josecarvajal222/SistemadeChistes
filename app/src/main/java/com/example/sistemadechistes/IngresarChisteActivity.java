9551faaf787ebd9ab42d668c942ac1b0e5669f1f
jose.carvajal22 <jacarvajalt@correo.udistrital.edu.co>
2019-03-30 09:00:25 -0500

    Declaración en la lógica para manejo de datos ingresados.

--- app/src/main/java/com/example/sistemadechistes/IngresarChisteActivity.java
+++ app/src/main/java/com/example/sistemadechistes/IngresarChisteActivity.java
@@ -4,10 +4,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class IngresarChisteActivity extends AppCompatActivity {
 
     Button subirChiste;
+
+    EditText tituloChiste;
+
+    EditText ingresoChiste;
+
+    EditText gradoOportuno;
+
+    EditText gradoNovedad;
+
+    EditText gradoDiversion;
+
+    EditText personajes;
+
+    EditText instrucciones;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +32,29 @@ public class IngresarChisteActivity extends AppCompatActivity {
 
         subirChiste = (Button)findViewById(R.id.ButtonSubirChiste);
 
+        tituloChiste = (EditText)findViewById(R.id.txtTituloChiste);
+
+        ingresoChiste = (EditText)findViewById(R.id.txtIngresoChiste);
+
+        gradoOportuno = (EditText)findViewById(R.id.txtGradoOportuno);
+
+        gradoNovedad = (EditText)findViewById(R.id.txtGradoNovedad);
+
+        gradoDiversion = (EditText)findViewById(R.id.txtGradoDiversion);
+
+        personajes = (EditText)findViewById(R.id.txtPersonajes);
+
+        instrucciones = (EditText)findViewById(R.id.txtInstrucciones);
+
         subirChiste.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
+
             }
         });
+
 
     }
 
